a720e94a8e0b97e112f8c78f5cfa11aea76f1858
Heon2 <adlgjs@naver.com>
2021-12-31 02:24:27 +0900

    first nodejs

---
+++ syntax/String.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('Of the nations of the world Korea alone, up to now, is the sole one which has risked its all against communism. The magnificence of the courage and fortitude of the Korean people defies descrption. They have chosen to risk death rather than slavery. Their last words to me were: Dont scuttle the Pacific.'.length);
